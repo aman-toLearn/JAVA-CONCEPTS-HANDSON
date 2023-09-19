adbb42670ff78101016dbcf08f356b36b6322286
aman8094 <realaman09@gmail.com>
2023-09-19 05:50:33 +0530

    created

---
+++ OOP/src/Intro01.java
@@ -0,0 +1,12 @@
+public class Intro01 {
+    String name;
+    int rollno;
+
+    public static void main(String[] args){
+
+        Intro01 objj = new Intro01();
+        System.out.println(objj.rollno);
+    }
+
+
+}
